3fb3e5f0f6e3f99fdac57412de0963c0bf8d3e6e
JxEbra25 <96464561+JxEbra25@users.noreply.github.com>
2021-12-21 09:12:21 +0200

    The second version
    we add another number

---
+++ somma/somma/src/somma/Somma.java
@@ -0,0 +1,20 @@
+
+package somma;
+
+
+public class Somma {
+
+   
+    public static void main(String[] args) {
+        
+        
+        int t=25;
+        int x=0;
+        int z=0;
+        int y=10;
+        z=x+y+t;
+        System.out.println("la somma è :"+z);
+        
+    }
+    
+}
