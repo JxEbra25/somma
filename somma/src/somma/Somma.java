72c4aef4b0373334c501f5af12e90496cd4752b0
Ahmedmahmoud1234 <77395730+Ahmedmahmoud1234@users.noreply.github.com>
2021-12-21 09:07:11 +0200

    first version

---
+++ somma/src/somma/Somma.java
@@ -0,0 +1,18 @@
+
+package somma;
+
+
+public class Somma {
+
+   
+    public static void main(String[] args) {
+        
+        int x=0;
+        int z=0;
+        int y=10;
+        z=x+y;
+        System.out.println(z);
+        
+    }
+    
+}
